ea9a8d48240890b712fd12826d8d34ed6c2f174c
CharleeWa <18888351756@163.com>
2024-01-13 23:27:11 +0800

    feat: Add shadcn-vue

---
+++ src/renderer/lib/utils.ts
@@ -0,0 +1,8 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+// import { camelize, getCurrentInstance, toHandlerKey } from 'vue'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
