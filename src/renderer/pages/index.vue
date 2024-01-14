007ea6e37a52635feaf59064989fcebf19165e26
CharleeWa <18888351756@163.com>
2024-01-14 17:56:55 +0200

    feat(wip): Add app left menus (#1)

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,7 +1,16 @@
-<script lang="ts" setup>
-
+<script setup lang="ts">
+import Sidebar from '@/components/Sidebar.vue'
 </script>
 
 <template>
-  <div />
+  <div class="border-t">
+    <div class="bg-background">
+      <div class="grid lg:grid-cols-5">
+        <Sidebar class="" />
+        <div class="col-span-3 lg:col-span-4 lg:border-l">
+          <div class="h-full px-4 py-6 lg:px-8" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
